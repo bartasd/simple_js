4ae402fbbb903f39296d90c96eb8efe90ec19660
Bartas Dausynas <bartas3@gmail.com>
2023-09-22 11:33:44 +0300

    rotation passes, not working as it should

--- index.js
+++ index.js
@@ -3,5 +3,19 @@ let element = document.getElementById("your_name");
 element.innerHTML = prompt("Please enter your name: ");
 
 // rotate text
+element.style.transform = `rotate3d(1, 1, 1, 180deg)`;
+let rotation = "";
 
-element.style.transform = 'rotate3d(1, 1, 1, 45deg)';
+const sleep = (time) => {
+  return new Promise((resolve) => setTimeout(resolve, time))
+}
+
+const doRotatiton = async () => {
+  for (let i = 0; i < 360; i++) {
+    rotation = step + "deg";
+    element.style.transform = `rotate3d(1, 1, 1, ${rotation})`;
+    await sleep(6000);
+  }
+}
+
+doRotation();
