68e9a77b01704c5a9e1360d65efabd9016abd402
Bartas Dausynas <bartas3@gmail.com>
2023-09-24 01:01:13 +0300

    finally sellected a tag

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
+// REMARKS: "element" should be initiated by const variable,
+// or it must be used inside the IIFE function...
+
 // PUT YOUR NAME ON THE SCREEN
-let element = document.getElementById("your_name");
-element.innerHTML = prompt("Please enter your name: ");
+const myName = prompt("Please enter your name: ");
+
+//const el1 = document.getElementById("q1");
+const element = document.querySelector('#q1 p');
+element.innerHTML = myName;
+
 
 // rotate text
-element.style.transform = `rotate3d(1, 1, 1, 180deg)`;
-let rotation = "";
+const keyframe = [
+  { transform: "rotate(0)" },
+  { transform: "rotate(360deg)" },
+];
 
-const sleep = (time) => {
-  return new Promise((resolve) => setTimeout(resolve, time))
+const effect = {
+  duration: 5000,
+  iterations: Infinity,
+};
+
+element.addEventListener("click", () => {
+  // https://developer.mozilla.org/en-US/docs/Web/API/Web_Animations_API/Keyframe_Formats
+  // keyframe keyframe options
+  element.animate(keyframe, effect);
+});
+
+/*
+function changeElement(arg) {
+  let quarter = (Math.floor(Math.random() * 100)) % 4 + 1;
+  quarter = "q" + quarter;
+  arg = document.getElementById(quarter);
+  arg.innerHTML = myName;
+  
 }
 
-const doRotatiton = async () => {
-  for (let i = 0; i < 360; i++) {
-    rotation = step + "deg";
-    element.style.transform = `rotate3d(1, 1, 1, ${rotation})`;
-    await sleep(6000);
-  }
-}
-
-doRotation();
+changeElement(element);
+*/
